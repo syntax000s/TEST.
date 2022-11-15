9b0539074e73080607fa56eeb0a60ec49d8551fe
syntax000s <112975910+syntax000s@users.noreply.github.com>
2022-11-15 22:07:14 +0700

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -1,36 +1,33 @@
 #include <iostream>
-void outputAnswer(int counter);
-int isEqualNumbers(int* number);
-int* inputNumber();
-int main() {
-	outputAnswer(isEqualNumbers(inputNumber()));
-	return 0;
+
+using namespace std;
+
+void output(string number){
+    cout<<number;
 }
-int* inputNumber() {
-	int number;
-	std::cin >> number;
-	return &number;
+
+void solution(string number){
+
+    for(int i = 0;i < number.size();i++){
+        for(int j = i + 1;j < number.size();j++){
+            if(number[i] == number[j]){
+                output("true");
+                return;
+            }
+        }
+    }
+    output("false");
 }
-int isEqualNumbers(int* number) {
-	int equalNumber;
-	int counter = 0;
-	int count = 0;
-	int arrayEqualNumbers[1024];
-	for (int i = 1; i < *number; i *= 10) {
-		if (i < 1000) equalNumber = (*number / i) % 10;
-		if (i > 999) equalNumber = *number / i;
-		arrayEqualNumbers[counter] = equalNumber;
-		counter++;
-	}
-	for (int i = 0; i < counter; i++) {
-		for (int j = 0; j < counter; j++) {
-			if (i == j) continue;
-			if (arrayEqualNumbers[i] == arrayEqualNumbers[j]) count++;
-		}
-	}
-	return count;
+
+void input(){
+    string number;
+    cin>>number;
+
+    solution(number);
 }
-void outputAnswer(int counter) {
-	if (counter != 0) std::cout << "true";
-	if (counter == 0) std::cout << "false";
+
+int main(){
+    input();
+    return 0;
 }
+
